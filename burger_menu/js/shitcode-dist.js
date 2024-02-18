d27d56a9b365dca6e909ebf92b8aa4a876b26c68
NikitaYatsko <144720850+NikitaYatsko@users.noreply.github.com>
2024-02-18 18:28:32 +0200

    Delete burger_menu directory

--- burger_menu/js/shitcode-dist.js
+++
@@ -1,1 +0,0 @@
-const menu=document.querySelector(".menu"),burger_menu=document.querySelector(".burger_menu");menu.addEventListener("click",(function(){burger_menu.classList.toggle("hidden")}));
--- burger_menu/js/shitcode.js
+++
@@ -1,5 +0,0 @@
-const menu = document.querySelector('.menu');
-const burger_menu = document.querySelector('.burger_menu');
-menu.addEventListener('click', function(){
-    burger_menu.classList.toggle('hidden');
-})
